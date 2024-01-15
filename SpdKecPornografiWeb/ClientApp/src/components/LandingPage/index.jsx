3e43c2fc9d401cbb420f50fe54aa34253372c492
Fadhlih Girindra Putra <fadhlihgp@gmail.com>
2024-01-16 06:26:56 +0700

    init

--- SpdKecPornografiWeb/ClientApp/src/components/LandingPage/index.jsx
+++ SpdKecPornografiWeb/ClientApp/src/components/LandingPage/index.jsx
@@ -1,9 +1,11 @@
 import {NavMenu} from "../navbar/NavMenu";
+import Footer from "../Footer";
 
 const LandingPage = () => {
     return <div>
         <NavMenu />
         <h1>This is landing page</h1>
+        <Footer />
     </div>
 }
 export default LandingPage;
