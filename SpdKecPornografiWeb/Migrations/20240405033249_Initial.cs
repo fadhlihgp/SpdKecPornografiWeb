8c584d95f6e82d6f45f483972fffea72ffb4be5b
Fadhlih Girindra Putra <fadhlihgp@gmail.com>
2024-04-05 11:32:52 +0700

    ds

---
+++ SpdKecPornografiWeb/Migrations/20240405033249_Initial.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpdKecPornografiWeb.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Otps",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OtpCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpiredAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsVerified = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Otps", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Fullname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastLogin = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsBlocked = table.Column<bool>(type: "bit", nullable: false),
+                    IsVerified = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Account_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Role",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Diagnosis",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "Text", nullable: true),
+                    Suggestion = table.Column<string>(type: "Text", nullable: true),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedById = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedById = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diagnosis", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Diagnosis_Account_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Diagnosis_Account_UpdatedById",
+                        column: x => x.UpdatedById,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Question",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuestionCode = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedById = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedById = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Question", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Question_Account_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Question_Account_UpdatedById",
+                        column: x => x.UpdatedById,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResultHistory",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TestCode = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DiagnosisId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AccountId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResultHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResultHistory_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResultHistory_Diagnosis_DiagnosisId",
+                        column: x => x.DiagnosisId,
+                        principalTable: "Diagnosis",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Answer",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnswerCode = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    QuestionId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedById = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedById = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Answer_Account_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Answer_Account_UpdatedById",
+                        column: x => x.UpdatedById,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Answer_Question_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Question",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Answer_Diagnosis",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AnswerId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    DiagnosisId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedById = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedById = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answer_Diagnosis", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Answer_Diagnosis_Account_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Answer_Diagnosis_Account_UpdatedById",
+                        column: x => x.UpdatedById,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Answer_Diagnosis_Answer_AnswerId",
+                        column: x => x.AnswerId,
+                        principalTable: "Answer",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Answer_Diagnosis_Diagnosis_DiagnosisId",
+                        column: x => x.DiagnosisId,
+                        principalTable: "Diagnosis",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { "1", "SuperAdmin" });
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { "2", "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { "3", "User" });
+
+            migrationBuilder.InsertData(
+                table: "Account",
+                columns: new[] { "Id", "CreatedAt", "Email", "Fullname", "ImageUrl", "IsActive", "IsBlocked", "IsVerified", "LastLogin", "Password", "PhoneNumber", "RoleId", "Username" },
+                values: new object[] { "1134636b-08cd-4306-85d9-3f9176befa77", new DateTime(2024, 4, 5, 10, 32, 49, 54, DateTimeKind.Local).AddTicks(364), "admin@email.com", "Admin", null, true, false, false, null, "$2a$12$zDIeyHL0Im6Mhet4TkAYb.CIFZfNCLFa2c4pg707FoJkGBLCGEuCi", "08123456227", "2", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "Account",
+                columns: new[] { "Id", "CreatedAt", "Email", "Fullname", "ImageUrl", "IsActive", "IsBlocked", "IsVerified", "LastLogin", "Password", "PhoneNumber", "RoleId", "Username" },
+                values: new object[] { "d941614b-4e34-42cc-bf68-f2f599c3cf85", new DateTime(2024, 4, 5, 10, 32, 49, 54, DateTimeKind.Local).AddTicks(357), "superadmin@email.com", "Super Admin", null, true, false, false, null, "$2a$12$zDIeyHL0Im6Mhet4TkAYb.CIFZfNCLFa2c4pg707FoJkGBLCGEuCi", "081234567", "1", "superadmin" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_Email",
+                table: "Account",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_PhoneNumber",
+                table: "Account",
+                column: "PhoneNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_RoleId",
+                table: "Account",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_Username",
+                table: "Account",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answer_AnswerCode",
+                table: "Answer",
+                column: "AnswerCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answer_CreatedById",
+                table: "Answer",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answer_QuestionId",
+                table: "Answer",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answer_UpdatedById",
+                table: "Answer",
+                column: "UpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answer_Diagnosis_AnswerId",
+                table: "Answer_Diagnosis",
+                column: "AnswerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answer_Diagnosis_CreatedById",
+                table: "Answer_Diagnosis",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answer_Diagnosis_DiagnosisId",
+                table: "Answer_Diagnosis",
+                column: "DiagnosisId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answer_Diagnosis_UpdatedById",
+                table: "Answer_Diagnosis",
+                column: "UpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Diagnosis_CreatedById",
+                table: "Diagnosis",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Diagnosis_UpdatedById",
+                table: "Diagnosis",
+                column: "UpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Question_CreatedById",
+                table: "Question",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Question_QuestionCode",
+                table: "Question",
+                column: "QuestionCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Question_UpdatedById",
+                table: "Question",
+                column: "UpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResultHistory_AccountId",
+                table: "ResultHistory",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResultHistory_DiagnosisId",
+                table: "ResultHistory",
+                column: "DiagnosisId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResultHistory_TestCode",
+                table: "ResultHistory",
+                column: "TestCode",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Answer_Diagnosis");
+
+            migrationBuilder.DropTable(
+                name: "Otps");
+
+            migrationBuilder.DropTable(
+                name: "ResultHistory");
+
+            migrationBuilder.DropTable(
+                name: "Answer");
+
+            migrationBuilder.DropTable(
+                name: "Diagnosis");
+
+            migrationBuilder.DropTable(
+                name: "Question");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+        }
+    }
+}
